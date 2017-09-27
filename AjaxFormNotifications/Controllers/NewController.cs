527b358139aa52002ba501537cdf0f62a954a073
steve <>
2017-09-27 11:13:14 +0100

    Fleshed out so that actual dismissable, "skinny" bootstrap alerts are returned and displayed, along with correct http status code on success / failure

--- AjaxFormNotifications/Controllers/NewController.cs
+++ AjaxFormNotifications/Controllers/NewController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.IO;
+using System.Web.Mvc;
 
 namespace AjaxFormNotifications.Controllers
 {
@@ -21,13 +22,57 @@ namespace AjaxFormNotifications.Controllers
                 return PartialView("_SuccessNotification", $"Saved value {data.Value}");
             }
 
-            //Response.StatusCode = 404;
+            Response.StatusCode = 404;
             return PartialView("_ErrorNotification", $"Failed to save value {data.Value}");
+        }
+
+        [HttpGet]
+        public ActionResult ShowJsonForm()
+        {
+            return View(new Data());
+        }
+
+        [HttpPost]
+        public JsonResult SaveJsonForm(Data data)
+        {
+            var even = (data.Value % 2) == 0;
+
+            if (even)
+            {
+                Response.StatusCode = 200;
+                return Json(new
+                {
+                    Message = $"Saved {data.Value}!",
+                    ViewHtml = this.RenderViewToString("_SuccessNotification", $"Saved value {data.Value}")
+                });
+            }
+
+            Response.StatusCode = 500;
+            return Json(new
+            {
+                Message = $"Could not save {data.Value}!",
+                ViewHtml = this.RenderViewToString("_ErrorNotification", $"Could not save value {data.Value}")
+            });
         }
     }
 
     public class Data
     {
         public int Value { get; set; }
+    }
+
+    public static class Extensions
+    {
+        public static string RenderViewToString(this Controller controller, string viewName, object model)
+        {
+            using (var writer = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(controller.ControllerContext, viewName);
+                controller.ViewData.Model = model;
+                var viewCxt = new ViewContext(controller.ControllerContext, viewResult.View, controller.ViewData, controller.TempData, writer);
+                viewCxt.View.Render(viewCxt, writer);
+                return writer.ToString();
+            }
+        }
     }
 }
--- AjaxFormNotifications/Scripts/app.js
+++ AjaxFormNotifications/Scripts/app.js
@@ -5,3 +5,13 @@
 function onFailure() {
     alert("Failure!");
 }
+
+function onJsonSuccess(data) {
+    console.log(`success! ${data.Message}`);
+    $("#my-target").prepend(data.ViewHtml);
+}
+
+function onJsonFailure(data) {
+    console.log(`error! ${data.responseJSON.Message}`);
+    $("#my-target").prepend(data.responseJSON.ViewHtml);
+}
